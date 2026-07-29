06ef5e237ec3d183f953ba301e3c8beb4bc79243
Manju-Patil-Appu <manjupatil2611@gmail.com>
2026-07-29 23:18:07 +0530

    Remove temporary database test script

--- backend/test_db.py
+++
@@ -1,8 +0,0 @@
-import psycopg
-
-conn = psycopg.connect(
-    "postgresql://postgres:YOUR_PASSWORD@db.hyjzlghlnscnlwstawzz.supabase.co:5432/postgres"
-)
-
-print("Connected successfully!")
-conn.close()
